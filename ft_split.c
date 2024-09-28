3c45cc71e7de000467539c7b316bcdf35354fe74
Pierre-Antoine Rigaudier <prigaudi@z1r7p5.42lyon.fr>
2024-09-28 18:39:16 +0200

    modif split to run with charset > 1

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,27 @@
 /*   By: prigaudi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 11:20:27 by prigaudi          #+#    #+#             */
-/*   Updated: 2024/09/28 16:16:29 by prigaudi         ###   ########.fr       */
+/*   Updated: 2024/09/28 18:35:38 by prigaudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+
+int	check_charset(char *str, char *charset, int i)
+{
+    int	j;
+
+	j = 0;
+	while (charset[j])
+	{
+		if (charset[j] != str[i + 1])
+			return (0);
+		i++;
+		j++;
+	}
+	return (1);
+}
 
 int	count_words(char *str, char *charset)
 {
@@ -22,9 +37,9 @@ int	count_words(char *str, char *charset)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] > 32 && str[i] < 127)
+		if (str[i] >= 32 && str[i] < 127)
 		{
-			if (str[i + 1] == charset[0])
+			if (check_charset(str, charset, i) == 1)
 				sum++;
 			if (str[i + 1] == '\0')
 				return (sum + 1);
@@ -37,12 +52,11 @@ int	count_words(char *str, char *charset)
 char	*save_word(char **tab, char *str, int *limit, int c)
 {
 	int	i;
-
 	tab[c] = malloc(sizeof(char) * (limit[1] - limit[0] + 2));
 	if (tab[c] == NULL)
 		return (NULL);
 	i = 0;
-	while (i <= limit[1] - limit[0] + 1)
+	while (i < limit[1] - limit[0] + 1)
 	{
 		tab[c][i] = str[limit[0] + i];
 		i++;
@@ -67,16 +81,18 @@ char	**ft_split(char *str, char *charset)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] >= 32 && str[i] < 127 && str[i] != charset[0])
+		if (str[i] >= 32 && str[i] < 127)
 		{
 			limit[0] = i;
-			while (str[i] >= 32 && str[i] < 127 && str[i] != charset[0])
+			while (str[i] >= 32 && str[i] < 127)
 			{
-				if (str[i + 1] == charset[0] || str[i + 1] == '\0')
+				if (check_charset(str, charset, i) == 1 || str[i + 1] == '\0')
 				{
-					limit[1] = i - 1;
+					limit[1] = i;
 					save_word(result, str, limit, c);
 					c++;
+					i++;
+					break ;
 				}
 				i++;
 			}
@@ -92,10 +108,12 @@ char	**ft_split(char *str, char *charset)
 	char	*str;
 	char	**test;
 	int		i;	
+	char	*charset;
 
-	str = "        Bonjour comment ca      va chez toi?      ";
-	test = ft_split(str, " ");
-	printf("Word count = %d\n", count_words(str));
+	charset = ":";
+	str = "        Bonjour  :    comment  :   ca    :  va: chez: toi?      ";
+	test = ft_split(str, charset);
+	printf("Word count = %d\n", count_words(str, charset));
 	i = 0;
 	while (i < 6)
 	{
