3a9fbd96713540432a48cbe72575c20de1d6f4be
elagouch <elagouch@42>
2024-09-28 11:36:31 +0200

    feat(pow.c): implement

--- ex00/pow.c
+++ ex00/pow.c
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush.h                                             :+:      :+:    :+:   */
+/*   pow.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elagouch <elagouch@42>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/28 11:13:44 by elagouch          #+#    #+#             */
-/*   Updated: 2024/09/28 11:35:01 by elagouch         ###   ########.fr       */
+/*   Created: 2024/09/28 11:33:45 by elagouch          #+#    #+#             */
+/*   Updated: 2024/09/28 11:36:07 by elagouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RUSH_H
-# define RUSH_H
-
-// Strings <-> numbers
-long	ft_atoi(char *str);
-// STDIN manipulations
-void	ft_putstr(char *str);
-// Strings manipulation
-char	*ft_strjoin(int size, char **strs, char *sep);
-char	*ft_strcat(char *dest, char *src);
-int	ft_strlen(char *str);
-// Numbers manipulation
-int	ft_pow(int nb, int pow);
-
-#endif
+/*
+ * Calculates `nb` to the power of `pow`.
+ */
+int	ft_pow(int nb, int pow)
+{
+	if (pow < 0)
+		return (0);
+	if (pow == 0)
+		return (1);
+	return (ft_pow(nb, pow - 1) * nb);
+}
