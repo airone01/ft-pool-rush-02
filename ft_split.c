767e63e402099b406ee678880f12ead7d3c1ec28
Pierre-Antoine Rigaudier <prigaudi@z1r7p5.42lyon.fr>
2024-09-28 16:11:13 +0200

    MAJ split and rush

--- ft_split.c
+++ ft_split.c
@@ -6,14 +6,14 @@
 /*   By: prigaudi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 11:20:27 by prigaudi          #+#    #+#             */
-/*   Updated: 2024/09/28 13:41:07 by prigaudi         ###   ########.fr       */
+/*   Updated: 2024/09/28 16:09:31 by prigaudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	count_words(char *str)
+int	count_words(char *str, char *charset)
 {
 	int	sum;
 	int	i;	
@@ -24,7 +24,7 @@ int	count_words(char *str)
 	{
 		if (str[i] > 32 && str[i] < 127)
 		{
-			if (str[i + 1] == ' ')
+			if (str[i + 1] == charset[0])
 				sum++;
 			if (str[i + 1] == '\0')
 				return (sum + 1);
@@ -34,17 +34,17 @@ int	count_words(char *str)
 	return (sum);
 }
 
-char	*save_word(char **tab, char *str, int *end, int c)
+char	*save_word(char **tab, char *str, int *limit, int c)
 {
 	int	i;
 
-	tab[c] = malloc(sizeof(char) * (end[1] - end[0] + 2));
+	tab[c] = malloc(sizeof(char) * (limit[1] - limit[0] + 2));
 	if (tab[c] == NULL)
 		return (NULL);
 	i = 0;
-	while (i <= end[1] - end[0] + 1)
+	while (i <= limit[1] - limit[0] + 1)
 	{
-		tab[c][i] = str[end[0] + i];
+		tab[c][i] = str[limit[0] + i];
 		i++;
 	}
 	tab[c][i] = '\0';
@@ -60,31 +60,33 @@ char	**ft_split(char *str, char *charset)
 	int		c;
 
 	c = 0;
-	nbr = count_words(str);
+	nbr = count_words(str, charset);
 	result = malloc(sizeof(char *) * nbr + 1);
 	if (result == NULL)
 		return (result);
 	i = -1;
 	while (str[++i])
 	{
-		if (str[i] > 32 && str[i] < 127)
+		if (str[i] >= 32 && str[i] < 127)
 		{
 			limit[0] = i;
-			while (str[++i] > 32 && str[i] < 127)
+			while (str[i] >= 32 && str[i] < 127)
 			{
-				if (str[i + 1] == ' ' || str[i + 1] == '\0')
+				if (str[i + 1] == charset[0] || str[i + 1] == '\0')
 				{
 					limit[1] = i;
 					save_word(result, str, limit, c);
 					c++;
 				}
+				i++;
 			}
 		}
 	}
+	result[c] = NULL; 
 	return (result);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	char	*str;
 	char	**test;
@@ -99,4 +101,4 @@ int	main(void)
 		printf("%s\n", test[i]);
 		i++;
 	}
-}
+}*/
---
+++ rush-02.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush-02.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prigaudi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/28 10:53:57 by prigaudi          #+#    #+#             */
+/*   Updated: 2024/09/28 15:38:42 by prigaudi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "ft_split.c"
+
+char    **ft_split(char *str, char *charset);
+
+int	main(void)
+{
+	int		file;
+	char	buffer[5000];
+	char	**dico1;
+	char	***dico2;
+	int		i;
+	int		len;
+
+	file = open("./numbers.dict", O_RDONLY);
+	read(file, buffer, 5000);
+	dico1 = ft_split(buffer, "\n");
+	len = 0;
+	while (dico1[i++])
+		len++;
+	dico2 = malloc(sizeof(char **) * len);
+	i = 0;
+	while (dico1[i])
+	{	
+		dico2[i] = ft_split(dico1[i],":");
+		printf("0: '%s', 1: '%s'\n", dico2[i][0], dico2[i][1]);
+		i++;
+	}
+}
